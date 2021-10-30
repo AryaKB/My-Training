5a9088fd1d156819dc085f942287ec378fd43ffd
AryaKB <93086435+AryaKB@users.noreply.github.com>
2021-10-30 10:29:35 +0530

    Delete My_Maven directory

--- My_Maven/src/main/java/maven/pack/MavenObj.java
+++
@@ -1,9 +0,0 @@
-package maven.pack;
-import java.io.IOException;
-public class MavenObj {
-	public static void main(String[] args) throws IOException {
-		 MavenClass obj=new  MavenClass();
-		 obj.read();
-		System.out.println(obj.readData(0, 1));
-	}
-}
